4fe639e478c0ab0437a603340f7f33b8b1421e20
Chris Gilmore <christopher.m.gilmore@gmail.com>
2022-08-31 18:49:14 -0400

    Things working

---
+++ .sfdx/typings/lwc/aim_check.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/aim_check" {
+    var aim_check: string;
+    export default aim_check;
+}
---
+++ .sfdx/typings/lwc/apex/AIM_EmailTemplateEditorController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/AIM_EmailTemplateEditorController.sendTestMailMethod" {
+  export default function sendTestMailMethod(param: {mMail: any, mSender: any, mSubject: any, mbody: any, templateId: any, whatId: any, saveAsActivity: any}): Promise<any>;
+}
+declare module "@salesforce/apex/AIM_EmailTemplateEditorController.getSenderAddresses" {
+  export default function getSenderAddresses(): Promise<any>;
+}
+declare module "@salesforce/apex/AIM_EmailTemplateEditorController.getEmailTempaltes" {
+  export default function getEmailTempaltes(): Promise<any>;
+}
