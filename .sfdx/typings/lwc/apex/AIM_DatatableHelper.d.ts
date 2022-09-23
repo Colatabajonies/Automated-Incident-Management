9eab331336b9bc4f72d2d0d07ff82ef13664485b
Chris Gilmore <christopher.m.gilmore@gmail.com>
2022-09-23 13:29:20 -0400

    Checkin for Vaibhav

--- .sfdx/typings/lwc/apex/AIM_DatatableHelper.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/AIM_DatatableHelper.getColumnString" {
-  export default function getColumnString(param: {tableData: any, fieldList: any}): Promise<any>;
-}
-declare module "@salesforce/apex/AIM_DatatableHelper.getTileData" {
-  export default function getTileData(param: {lstInput: any}): Promise<any>;
-}
--- .sfdx/typings/lwc/apex/GeoMapController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/GeoMapController.getAssets" {
-  export default function getAssets(param: {inputIds: any}): Promise<any>;
-}
